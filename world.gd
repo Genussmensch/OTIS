ad968af7af224c9f2f809d69ed8d4c107483b8f5
BabakAnvari <b.anvari@web.de>
2023-12-09 20:52:43 +0100

    OTIS
    maybe now?

---
+++ world.gd
@@ -0,0 +1,14 @@
+extends Node3D
+@onready var music =$music
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	music.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
